b50bcd9bd95ec09a22c657827dcba4500a3ce8db
correenee <kkim509@naver.com>
2020-01-02 22:13:27 +0900

    server commit

---
+++ TikTok/TikTok/New Group/CategoryJunsung/Service/APIService.swift
@@ -0,0 +1,33 @@
+//
+//  APIService.swift
+//  TikTok
+//
+//  Created by 김준성 on 2020/01/02.
+//  Copyright © 2020 김준성. All rights reserved.
+//
+
+import Moya
+
+final class APIService {
+    static let shared = APIService()
+    private init() {}
+    
+    private let provider = MoyaProvider<APITarget>()
+    
+    func requestMedianHotel(cityID: Int, completion: @escaping (Result<MedianHotelResponseModel, Error>) -> Void) {
+        provider.request(.medianHotelRead(cityID: cityID)) { result in
+            switch result {
+            case let .success(success):
+                let responseData = success.data
+                do {
+                    let decoded = try JSONDecoder().decode(MedianHotelResponseModel.self, from: responseData)
+                    completion(.success(decoded))
+                } catch {
+                    completion(.failure(error))
+                }
+            case let .failure(error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
